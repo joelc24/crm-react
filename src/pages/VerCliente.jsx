fdb1794075b53dab8c2d86fd89d9dc5e4c2b0333
Joel <libra-marriaga@hotmail.com>
2022-03-13 16:55:49 -0500

    Mostrando la informacion de un cliente por su id

--- src/pages/VerCliente.jsx
+++ src/pages/VerCliente.jsx
@@ -1,12 +1,55 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 
 const VerCliente = () => {
 
-    const {id} = useParams()
+    const [cliente, setCliente] = useState([]);
 
+    const {id} = useParams()
+    useEffect(() => {
+        const obtenerClienteAPI = async () =>{
+            try {
+                const url =`http://localhost:4000/clientes/${id}`
+                const respuesta = await fetch(url)
+                const resultado = await respuesta.json()
+                setCliente(resultado)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        obtenerClienteAPI()
+    }, []);
     return ( 
         <div>
-            <h1>VerCliente.jsx</h1>
+            <h1 className="font-black text-4xl text-blue-900">Ver Cliente: {cliente.nombre}</h1>
+            <p className="mt-3">Informacion del Cliente</p>
+
+            <p className='text-2xl text-gray-600 mt-10'>
+                <span className='text-gray-800 uppercase font-bold'>Cliente: </span>
+                {cliente.nombre}
+            </p>
+            <p className='text-2xl text-gray-600 mt-4'>
+                <span className='text-gray-800 uppercase font-bold'>Email: </span>
+                {cliente.email}
+            </p>
+            {cliente.telefono && (
+            <p className='text-2xl text-gray-600 mt-4'>
+                <span className='text-gray-800 uppercase font-bold'>Telefono: </span>
+                {cliente.telefono}
+            </p>
+
+            )}
+            <p className='text-2xl text-gray-600 mt-4'>
+                <span className='text-gray-800 uppercase font-bold'>Empresa: </span>
+                {cliente.empresa}
+            </p>
+            {cliente.notas && (
+                <p className='text-2xl text-gray-600 mt-4'>
+                    <span className='text-gray-800 uppercase font-bold'>Notas: </span>
+                    {cliente.notas}
+                </p>
+
+            )}
         </div>
      );
 }
