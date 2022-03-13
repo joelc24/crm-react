4863d073d59a6ea2abb4c55f980b989cde2ea92c
Joel <libra-marriaga@hotmail.com>
2022-03-13 12:22:23 -0500

    el hook use location

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,5 +1,7 @@
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 const Layout = () => {
+    const location = useLocation()
+    const urlActual = location.pathname
     return ( 
         <div className='md:flex md:min-h-screen'>
             <div className='md:w-1/4 bg-blue-900 px-5 py-10'>
@@ -7,11 +9,11 @@ const Layout = () => {
                 <nav className='mt-10'>
                     <Link 
                         to="/clientes"
-                        className='text-white text-2xl block mt-2 hover:text-blue-300'
+                        className={` ${urlActual === '/clientes' ? 'text-blue-300' : ''} 9text-white text-2xl block mt-2 hover:text-blue-300`}
                     >Clientes</Link>
                     <Link 
                         to="/clientes/nuevo"
-                        className='text-white text-2xl block mt-2 hover:text-blue-300'
+                        className={`${urlActual === '/clientes/nuevo' ? 'text-blue-300': ''} text-white text-2xl block mt-2 hover:text-blue-300`}
                     >Nuevo Cliente</Link>
 
                 </nav>
