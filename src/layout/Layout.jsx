374183aecbb7e14a8927418eba29c401309da939
Joel <libra-marriaga@hotmail.com>
2022-03-13 12:11:16 -0500

    El componente link

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,20 +1,20 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 const Layout = () => {
     return ( 
         <div className='md:flex md:min-h-screen'>
             <div className='md:w-1/4 bg-blue-900 px-5 py-10'>
                 <h2 className='text-4xl font-black text-center text-white'>CRM Clientes</h2>
-                <div className='mt-10'>
-                    <a 
-                        href="/clientes"
-                        className='text-white text-2xl block mt-2 hover:blue-300'
-                    >Clientes</a>
-                    <a 
-                        href="/cientes/nuevo"
-                        className='text-white text-2xl block mt-2 hover:blue-300'
-                    >Nuevo Cliente</a>
+                <nav className='mt-10'>
+                    <Link 
+                        to="/clientes"
+                        className='text-white text-2xl block mt-2 hover:text-blue-300'
+                    >Clientes</Link>
+                    <Link 
+                        to="/clientes/nuevo"
+                        className='text-white text-2xl block mt-2 hover:text-blue-300'
+                    >Nuevo Cliente</Link>
 
-                </div>
+                </nav>
             </div>
             <div className='md:w-3/4'>
                 <Outlet/>
