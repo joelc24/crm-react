d86ff3a35b9d0a8e4a85ee5aacecd0bb086c62df
Joel <libra-marriaga@hotmail.com>
2022-03-13 19:09:49 -0500

    Mostrando textos de forma condicional

--- src/components/Fromulario.jsx
+++ src/components/Fromulario.jsx
@@ -47,7 +47,7 @@ const Formulario = ({cliente}) => {
     }
     return ( 
         <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto">
-            <h1 className="text-gray-600 font-bold text-xl uppercase text-center">Agregar Clientes</h1>
+            <h1 className="text-gray-600 font-bold text-xl uppercase text-center">{cliente?.nombre ? "Editar Cliente" : "Agregar Cliente"}</h1>
             <Formik
                 initialValues={{
                     nombre: cliente?.nombre ?? "",
@@ -148,7 +148,7 @@ const Formulario = ({cliente}) => {
                     </div>
                     <input 
                         type="submit" 
-                        value='Agregar Nuevo Cliente'
+                        value={cliente?.nombre ? "Editar Cliente" : "Agregar Cliente"}
                         className='mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg cursor-pointer hover:bg-blue-900'
                     />
                 </Form>
